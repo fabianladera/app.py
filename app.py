ef614ef6cb005f232fa4e8cef374d6dc15aeedef
fabianladera <fabian.ladera.d@gmail.com>
2026-05-12 19:48:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ st.title("Mi primera aplicación en python")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Carlos Carrillo")
+
+sesion = st.selectbox("Selecciones una sesion",["Sesion 1 ","Sesion 2","Sesion 3","Sesion 4"]
